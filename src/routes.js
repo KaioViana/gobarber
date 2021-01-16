8dc64c294e59480e0fec91016b8552227639dcbe
KaioViana <kaiu.viana@hotmail.com>
2021-01-16 05:33:32 -0300

    estrutura de pastas feita, conexão com o banco feita, teste de conexão executado

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
+import User from './app/models/User'
 
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    return res.json({message: 'Hello World'})
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Kaio',
+        email: 'kaio_email',
+        password_hash: '11234566'
+    })
+
+    return res.json({user})
 })
 
 export default routes
