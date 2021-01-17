116e8e8d78f57ac57889790c6c26a59a79c61491
KaioViana <kaiu.viana@hotmail.com>
2021-01-17 05:55:22 -0300

    cadastro de usuários feito

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express'
-import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 
 
 const routes = new Router()
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Kaio',
-        email: 'kaio_email',
-        password_hash: '11234566'
-    })
-
-    return res.json({user})
-})
+routes.post('/users', UserController.store)
 
 export default routes
