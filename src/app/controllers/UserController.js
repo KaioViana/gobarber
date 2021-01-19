e0015e041e39e2f154417b6ae814275f17827aa2
KaioViana <kaiu.viana@hotmail.com>
2021-01-19 04:08:28 -0300

    codigo organizado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,16 +3,10 @@ import User from '../models/User'
 
 class UserController {
     async store(req, res) {
-        const userExists = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
+        const userExists = await User.findOne({ where: { email: req.body.email } })
 
         if (userExists) {
-            return res.status(400).json({
-                error: 'User already exists.'
-            })
+            return res.status(400).json({ error: 'User already exists.' })
         }
 
         const { id, name, email, provider } = await User.create(req.body)
