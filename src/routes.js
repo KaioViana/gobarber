69d1ee5b639c0c9caef5bbce25b2f31b646f37d8
KaioViana <kaiu.viana@hotmail.com>
2021-01-22 07:07:32 -0300

    criaçao de avatar feita, listagem de providers feita, acesso statico a rota files feito

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
+import authMiddleware from './app/middlewares/auth'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
-import authMiddleware from './app/middlewares/auth'
+import FileController from './app/controllers/FileController'
+import ProviderController from './app/controllers/ProviderController'
+
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware)
 routes.put('/users', UserController.update)
+routes.post('/files', upload.single('file'), FileController.store)
+routes.get('/providers', ProviderController.index)
 
 export default routes
