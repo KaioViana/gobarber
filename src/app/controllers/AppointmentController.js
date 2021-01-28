bf88ade90d6fc196fd3fb8a59c4376e6ba3900ae
Kaio Viana <kaiu.viana@hotmail.com>
2021-01-28 19:14:37 -0300

    appointments finalizado

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 import { startOfHour, parseISO, isBefore } from 'date-fns'
 import Appointment from "../models/Appointment"
 import User from '../models/User'
+import File from '../models/File'
 
 
 class AppointmentController {
@@ -51,6 +52,30 @@ class AppointmentController {
         })
 
         return res.json(appointment)
+    }
+
+    async index(req, res) {
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url']
+                        }
+                    ]
+                }
+            ]
+        })
+
+        return res.json(appointments)
     }
 }
 
