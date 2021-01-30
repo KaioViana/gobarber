130ea3b311bec94c8f5130ebb27cf70c38cd5ceb
Kaio Viana <kaiu.viana@hotmail.com>
2021-01-30 13:17:53 -0300

    configuraçao de templates de email feita

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from "../models/Appointment"
 import User from '../models/User'
 import File from '../models/File'
 import Notification from '../schemas/Notification'
+import Mail from '../../lib/Mail'
 
 
 class AppointmentController {
@@ -96,7 +97,20 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id)
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email']
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name']
+                }
+            ]
+        })
 
         if(appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -115,6 +129,19 @@ class AppointmentController {
         appointment.canceled_at = new Date()
 
         await appointment.save()
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(appointment.date, "'dia' dd 'de' MMMM', às' h:mm'h'", {
+                    locale: pt
+                })
+            }
+        })
 
         return res.json(appointment)
     }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '78cbbc86923c5f',
+        pass: '44c97dde56590b'
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarer.com>'
+    }
+}
