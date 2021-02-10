105cacbfe53f478c7cd381e0bc384941b7dc38be
Kaio Viana <kaiu.viana@hotmail.com>
2021-02-10 17:21:02 -0300

    Validações dos campos finalizadas

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '~/assets/logo.svg'
+import { Form, Input } from '@rocketseat/unform'
+import * as Yup from 'yup'
 
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string()
+        .required('A senha é obrigatória')
+})
 
 export default function SignIn() {
+    function handleSubmit(data) {
+        console.tron.log(data)
+    }
+
     return (
         <>
             <img src={ logo } alt="GoBarber"/>
 
-            <form>
-                <input type="email" placeholder="Seu email" />
-                <input type="password" placeholder="Sua senha secreta" />
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="email" type="email" placeholder="Seu email" />
+                <Input name="password" type="password" placeholder="Sua senha secreta" />
 
                 <button type="subimit">Acessar</button>
                 <Link to="/register">Criar conta gratuita</Link>
-            </form>
+            </Form>
         </>
     )
 }
--- web/src/pages/_layouts/auth/styles.js
+++ web/src/pages/_layouts/auth/styles.js
@@ -19,46 +19,55 @@ export const Content = styled.div`
         display: flex;
         flex-direction: column;
         margin-top: 30px;
-    }
 
-    input {
-        background: rgba(0, 0, 0, 0.1);
-        border: 0;
-        border-radius: 4px;
-        height: 44px;
-        padding: 0 15px;
-        color: #FFF;
-        margin: 0 0 10px;
+        input {
+            background: rgba(0, 0, 0, 0.1);
+            border: 0;
+            border-radius: 4px;
+            height: 44px;
+            padding: 0 15px;
+            color: #FFF;
+            margin: 0 0 10px;
+    
+            &::placeholder {
+                color: rgba(255, 255, 255, 0.7);
+            } 
+        }
 
-        &::placeholder {
-            color: rgba(255, 255, 255, 0.7);
-        } 
-    }
-
-    button {
-        margin: 5px 0 0;
-        height: 44px;
-        background: #3b9eff;
-        font-weight: bold;
-        color: #FFF;
-        border: 0;
-        border-radius: 4px;
-        font-size: 16px;
-        transition: 0.2s
-
-        &:hover {
-            background: ${darken(0.03, '#3b9eff')}
+        span {
+            color: #fb6f91;
+            align-self: flex-start;
+            margin: 0 0 10px;
+            font-weight: bold;
+        }
+    
+        button {
+            margin: 5px 0 0;
+            height: 44px;
+            background: #3b9eff;
+            font-weight: bold;
+            color: #FFF;
+            border: 0;
+            border-radius: 4px;
+            font-size: 16px;
+            transition: 0.2s
+    
+            &:hover {
+                background: ${darken(0.03, '#3b9eff')}
+            }
+        }
+    
+        a {
+            color: #FFF;
+            margin-top: 15px;
+            font-size: 16px;
+            opacity: 0.8;
+    
+            &:hover {
+                opacity: 1;
+            }
         }
     }
 
-    a {
-        color: #FFF;
-        margin-top: 15px;
-        font-size: 16px;
-        opacity: 0.8;
-
-        &:hover {
-            opacity: 1;
-        }
-    }
+    
 `
