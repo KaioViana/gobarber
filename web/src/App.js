da1134d6685c09f47eacf5ba62d1bbdfc2d112a9
Kaio Viana <kaiu.viana@hotmail.com>
2021-03-04 17:21:20 -0300

    Persistencia de autenticação finalizada

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import './config/ReactotronConfig'
-import store from './store'
+import { store, persistor } from './store'
 import Routes from './routes'
 import history from './services/history'
 import GlobalStyle from './styles/global'
@@ -11,10 +12,12 @@ import GlobalStyle from './styles/global'
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
     
   )
